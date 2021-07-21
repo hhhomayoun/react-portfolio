05e5057ed33e635e0d14811dfcbc3d72b016a395
hhhomayoun <hhhomayoun@gmail.com>
2021-07-21 12:13:11 -0700

    minor update

--- src/About.js
+++ src/About.js
@@ -4,11 +4,12 @@ const About = () => {
       <h2>about me</h2>
       <div className='about-info'>
         <p>
-          My name is Homayoun, I live in Los Angeles and I am a front end web
-          developer. My educational background is BS in Mechanical Engineering
-          from California state University Northridge. I love coding and I work
-          with JavaScript and React.js on a day to day basis to create awsome
-          and exciting web applications.
+          My name is Homayoun, I live in Los Angeles. I Have been learning front
+          end development by creating web applications for about 2 years and I'm
+          ready to start my journey as a developer. My educational background is
+          BS in Mechanical Engineering from California state University
+          Northridge. I love coding and I work with JavaScript and React.js on a
+          day to day basis to create awsome and exciting web applications.
         </p>
       </div>
     </div>
